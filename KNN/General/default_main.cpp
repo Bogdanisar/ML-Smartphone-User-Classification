54004125e7ee8f6a50d57bc995d072d59ddf9e9a
Bogdan <burcea.bogdan.madalin@gmail.com>
2019-05-10 16:37:28 +0300

    added svm code with linear and rbf kernels; rbf has multiple tests done

---
+++ KNN/General/default_main.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#if 1
+    #define pv(x) std::cerr<<#x<<" = "<<(x)<<"; ";std::cerr.flush()
+    #define pn std::cerr<<std::endl
+#else
+    #define pv(x)
+    #define pn
+#endif
+
+#ifdef INFOARENA
+    ifstream in(".in");
+    ofstream out(".out");
+#else
+    #define in cin
+    #define out cout
+#endif
+
+using ll = long long;
+using ull = unsigned long long;
+using uint = unsigned int;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using pld = pair<ld, ld>;
+#define pb push_back
+const double PI = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862;
+const int inf_int = 1e9 + 5;
+const ll inf_ll = 1e18 + 5;
+const int NMax = 1e2 + 5;
+const int dx[] = {-1,0,0,+1}, dy[] = {0,-1,+1,0};
+const double EPS = 1e-8;
+
+int main() {
+    cin.sync_with_stdio(false);
+    cin.tie(0);
+    
+    cout << "hi!\n";
+
+    return 0;
+}
